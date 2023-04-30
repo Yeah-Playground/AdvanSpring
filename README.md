1545f9159a87e4df1d7705e4445c247945f413d4
YEAHx4 <developerstarlight@gmail.com>
2023-04-30 20:51:58 +0900

    handling GET request

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # AdvanSpring
-It's spring play with spring
+Spring을 독학해보면서 작성한 프로젝트입니다.
+어떤 목적보다는 Spring에 있는 여러 기능을 살펴보기 위해 작성되었습니다.
+
+## Dependency
+`@RestController`, `@GetMapping` 등의 web 기능을 이용하려면 아레의 dependency가 필요함
+```kt
+implementation("org.springframework.boot:spring-boot-starter-web")
+```
+
+## Reference
+까먹을때마다 참고할 수 있는 index
+
+- GET 요청 : HelloController
---
+++ src/main/kotlin/io/yeahx4/advanspring/controller/HelloController.kt
@@ -0,0 +1,46 @@
+package io.yeahx4.advanspring.controller
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class HelloController {
+    @GetMapping("/hello")
+    fun hello() = "Hello World"
+
+    @GetMapping("/hello/name/{name}")
+    fun helloName(@PathVariable name: String) = "Hello $name"
+
+    @GetMapping("/hello/param")
+    fun helloParam(
+        @RequestParam name: String,
+        @RequestParam email: String,
+        @RequestParam corp: String
+    ) = "Hello $name in $corp. Your email is $email"
+
+    // 미리 알지 못하는 여러개의 param을 Map으로 관리하는 방법
+    @GetMapping("/hello/unknown")
+    fun helloParams(@RequestParam param: Map<String, String>): String {
+        val sb = StringBuilder()
+
+        param.entries.forEach {
+            sb.append("${it.key} : ${it.value}\n")
+        }
+
+        return sb.toString()
+    }
+
+    // DTO
+    data class MemberDto(
+        val name: String,
+        val email: String,
+        val corp: String
+    ) {
+        override fun toString(): String = "$name - $email : ${corp}소속"
+    }
+
+    @GetMapping("/hello/dto")
+    fun helloDto(dto: MemberDto) = dto.toString()
+}
