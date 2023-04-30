7aecaaf75d177096175a19c35dac48b1f50e8d8e
YEAHx4 <developerstarlight@gmail.com>
2023-04-30 18:21:53 +0900

    Init Spring boot

---
+++ src/main/kotlin/io/yeahx4/advanspring/AdvanSpringApplication.kt
@@ -0,0 +1,11 @@
+package io.yeahx4.advanspring
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AdvanSpringApplication
+
+fun main(args: Array<String>) {
+    runApplication<AdvanSpringApplication>(*args)
+}
